845ea7b1f7e99034c0d13002c70fa73f00d3d0d0
lenomed <lenomedmega@gmail.com>
2026-05-24 14:03:50 +0100

    chore

--- J_wt/auth_util.py
+++ J_wt/auth_util.py
@@ -20,7 +20,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
     expire = datetime.utcnow()+(expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_LIFECYCLE))
     to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, SECRETE_KEY, algorithm = ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRETE_KEY, algorithm = [ALGORITHM])
     return encoded_jwt
 
 def decode_access_token(token: str):
